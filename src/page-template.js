b30b47e9ec9211fd6976cfab9882074bf52bfb03
unknown <viakeegan@gmail.com>
2022-01-10 12:48:49 -0500

    create engineer card

--- src/page-template.js
+++ src/page-template.js
@@ -18,4 +18,21 @@ const createManager = manager => {
   }
 
   // create engineer card
-  
+  const createEngineer = engineer => {
+    return `
+      <div class="shadow col-2 flex-column m-2 p-0">
+        <div class="bg-info px-2 pb-1">
+          <h2>${engineer.name}</h2>
+          <h3>Engineer</h3>
+        </div>
+        
+        <div class="flex-wrap flex-column p-0">
+          <ul class="list-unstyled m-0 p-2">
+            <li class="bg-white border border-dark px-1">ID: ${engineer.id}</li>
+            <li class="bg-white border border-dark px-1">Email: ${engineer.email}</li>
+            <li class="bg-white border border-dark px-1">Github username: ${engineer.github}</li>
+          </ul>
+        </div>
+      </div>
+    `
+  }
